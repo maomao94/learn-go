5b2155635b63573d34a25c4cb1e67ba721d399bf
hehanpeng <hanpeng.he@polixir.ai>
2020-10-19 15:08:40 +0800

    统一错误处理

--- errorhandling/filelistingserver/web.go
+++ errorhandling/filelistingserver/web.go
@@ -1,27 +1,37 @@
 package main
 
 import (
-	"io/ioutil"
+	"learn-go/errorhandling/filelistingserver/filelisting"
 	"net/http"
 	"os"
 )
 
+type appHandler func(writer http.ResponseWriter,
+	request *http.Request) error
+
+func errWrapper(handler appHandler) func(w http.ResponseWriter,
+	r *http.Request) {
+	return func(writer http.ResponseWriter,
+		request *http.Request) {
+		err := handler(writer, request)
+		if err != nil {
+			code := http.StatusOK
+			switch {
+			case os.IsExist(err):
+				code = http.StatusNotFound
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(writer,
+				http.StatusText(code),
+				code)
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/list/",
-		func(writer http.ResponseWriter,
-			request *http.Request) {
-			path := request.URL.Path[len("/list/"):] // /list/fib.txt
-			file, err := os.Open(path)
-			if err != nil {
-				panic(err)
-			}
-			defer file.Close()
-			all, err := ioutil.ReadAll(file)
-			if err != nil {
-				panic(err)
-			}
-			writer.Write(all)
-		})
+		errWrapper(filelisting.HandleFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
