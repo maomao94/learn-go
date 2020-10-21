4b98e098310f7b5f2dcab9f77e440c10b71f0739
hehanpeng <hanpeng.he@polixir.ai>
2020-10-21 15:12:15 +0800

    服务器统一处理2

--- errorhandling/filelistingserver/filelisting/handler.go
+++ errorhandling/filelistingserver/filelisting/handler.go
@@ -13,9 +13,8 @@ const prefix = "/list/"
 func HandleFileList(writer http.ResponseWriter,
 	request *http.Request) error {
 	if strings.Index(request.URL.Path, prefix) != 0 {
-		return errors.New("path must start with" + prefix)
+		return errors.New("path must start with " + prefix)
 	}
-
 	path := request.URL.Path[len(prefix):] // /list/fib.txt
 	file, err := os.Open(path)
 	if err != nil {
