4e5c68cb8327c8aa7d60b958c0a26b352706c948
hehanpeng <hanpeng.he@polixir.ai>
2020-10-27 11:09:42 +0800

    测试http服务器

---
+++ errorhandling/filelistingserver/errWrapper_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+)
+
+func errPanic(writer http.ResponseWriter,
+	request *http.Request) error {
+	panic(123)
+}
+
+type testingUserError string
+
+func (e testingUserError) Error() string {
+	return e.Message()
+}
+
+func (e testingUserError) Message() string {
+	return string(e)
+}
+
+func errUserError(writer http.ResponseWriter,
+	request *http.Request) error {
+	return testingUserError("user error")
+}
+
+func errNotFound(writer http.ResponseWriter,
+	request *http.Request) error {
+	return os.ErrNotExist
+}
+
+func errNotPermission(writer http.ResponseWriter,
+	request *http.Request) error {
+	return os.ErrPermission
+}
+
+func errUnknown(writer http.ResponseWriter,
+	request *http.Request) error {
+	return errors.New("unknown error")
+}
+
+func TestErrWrapper(t *testing.T) {
+	tests := []struct {
+		h       appHandler
+		code    int
+		message string
+	}{
+		{errPanic, 500, "Internal Server Error"},
+		{errUserError, 400, "user error"},
+		{errNotFound, 500, "Internal Server Error"},
+		{errNotPermission, 403, "Forbidden"},
+		{errUnknown, 500, "Internal Server Error"},
+	}
+
+	for _, tt := range tests {
+		f := errWrapper(tt.h)
+		response := httptest.NewRecorder()
+		request := httptest.NewRequest(
+			http.MethodGet, "http://www.baidu.com", nil)
+		f(response, request)
+		b, _ := ioutil.ReadAll(response.Body)
+		body := strings.Trim(string(b), "\n")
+		if response.Code != tt.code ||
+			body != tt.message {
+			t.Errorf("expect (%d, %s); "+
+				"got (%d, %s)",
+				tt.code, tt.message, response.Code, body)
+		}
+	}
+}
