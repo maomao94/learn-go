ecc43289e65fa26599a8d1ac3aafd4e535d9be1a
hehanpeng <hanpeng.he@polixir.ai>
2020-10-28 17:35:51 +0800

    channel

--- channel/channel.go
+++ channel/channel.go
@@ -5,17 +5,25 @@ import (
 	"time"
 )
 
+func worker(id int, c chan int) {
+	for {
+		//n := <-c
+		fmt.Printf("Worker %d receiver %c\n", id, <-c)
+	}
+}
 func chanDemo() {
 	//var c chan int // c == nil
-	c := make(chan int)
-	go func() {
-		for {
-			n := <-c
-			fmt.Println(n)
-		}
-	}()
-	c <- 1
-	c <- 2
+	var channels [10]chan int
+	for i := 0; i < 10; i++ {
+		channels[i] = make(chan int)
+		go worker(i, channels[i])
+	}
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + i
+	}
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'A' + i
+	}
 	time.Sleep(time.Millisecond)
 }
 
