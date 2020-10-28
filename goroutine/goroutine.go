911bfd82f6323913aa61d7dd13222023e5f24ac5
hehanpeng <hanpeng.he@polixir.ai>
2020-10-28 17:18:07 +0800

    go语言的调度器

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"time"
 )
 
 func main() {
 	var a [10]int
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100000; i++ {
 		go func(i int) {
 			for {
-				a[i]++
-				runtime.Gosched() //主动交出控制权
-				//fmt.Printf("Hello from goroutine %d\n", i)
+				//a[i]++
+				//runtime.Gosched() //主动交出控制权
+				fmt.Printf("Hello from goroutine %d\n", i)
 			}
 		}(i)
 	}
-	time.Sleep(time.Millisecond)
+	time.Sleep(time.Minute)
 	fmt.Println(a)
 }
