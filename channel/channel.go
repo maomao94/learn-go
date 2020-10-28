fd0365da5df8c55a82c81d877caf945b3330897a
hehanpeng <hanpeng.he@polixir.ai>
2020-10-28 17:39:03 +0800

    channel

--- channel/channel.go
+++ channel/channel.go
@@ -11,12 +11,25 @@ func worker(id int, c chan int) {
 		fmt.Printf("Worker %d receiver %c\n", id, <-c)
 	}
 }
+
+func createWorker(id int) chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			//n := <-c
+			fmt.Printf("Worker %d receiver %c\n", id, <-c)
+		}
+	}()
+	return c
+}
+
 func chanDemo() {
 	//var c chan int // c == nil
 	var channels [10]chan int
 	for i := 0; i < 10; i++ {
-		channels[i] = make(chan int)
-		go worker(i, channels[i])
+		//channels[i] = make(chan int)
+		//go worker(i, channels[i])
+		channels[i] = createWorker(i)
 	}
 	for i := 0; i < 10; i++ {
 		channels[i] <- 'a' + i
