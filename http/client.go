c9780e89c54e0f890bac230121fa7058745b9265
hehanpeng <hanpeng.he@polixir.ai>
2020-10-30 16:58:10 +0800

    http标准库

--- http/client.go
+++ http/client.go
@@ -7,9 +7,16 @@ import (
 )
 
 func main() {
-	request, err := http.NewRequest(http.MethodGet, "https://www.imooc.com/", nil)
+	request, err := http.NewRequest(http.MethodGet, "http://www.imooc.com/", nil)
 	request.Header.Add("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Mobile Safari/537.36")
-	resp, err := http.DefaultClient.Do(request)
+	//resp, err := http.DefaultClient.Do(request)
+	client := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("Redirect:", req)
+			return nil
+		},
+	}
+	resp, err := client.Do(request)
 	if err != nil {
 		panic(err)
 	}
