debcbcadd39339cdd81bfd022e1de92ff0375bb1
hehanpeng <hanpeng.he@polixir.ai>
2020-10-30 16:51:14 +0800

    http标准库

---
+++ http/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	request, err := http.NewRequest(http.MethodGet, "https://www.imooc.com/", nil)
+	request.Header.Add("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Mobile Safari/537.36")
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	s, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", s)
+}
