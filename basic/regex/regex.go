7c4da1948b961b7ab44eccaa8e8343d95088004c
hehanpeng <hanpeng.he@polixir.ai>
2020-11-02 17:48:58 +0800

    正则表达式

---
+++ basic/regex/regex.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+const text = `
+My email is ccc@gmail.com
+email is abc@def.org
+emai is aaaaqq@aaa.com.aa
+`
+
+func main() {
+	re := regexp.MustCompile(`([a-zA-Z0-9]+)@([a-zA-Z0-9.]+)\.([a-zA-Z0-9]+)`)
+	match := re.FindAllStringSubmatch(text, -1)
+	for _, m := range match {
+		fmt.Println(m)
+	}
+}
