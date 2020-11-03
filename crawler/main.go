2866e88afbbea327b4700dd4299e1fe39e4c3f35
hehanpeng <hanpeng.he@polixir.ai>
2020-11-03 11:18:26 +0800

    提取城市盒url

--- crawler/main.go
+++ crawler/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 
 	"golang.org/x/text/transform"
 
@@ -34,7 +35,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n", all)
+
+	printCityList(all)
+	//fmt.Printf("%s\n", all)
 }
 
 func determineEncoding(
@@ -45,4 +48,13 @@ func determineEncoding(
 	}
 	e, _, _ := charset.DetermineEncoding(bytes, "")
 	return e
+}
+
+func printCityList(content []byte) {
+	re := regexp.MustCompile(`<a href="(http://localhost:8080/mock/www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	matches := re.FindAllSubmatch(content, -1)
+	for _, m := range matches {
+		fmt.Printf("City: %s,URL: %s\n", m[2], m[1])
+	}
+	fmt.Printf("Matches found: %d\n", len(matches))
 }
