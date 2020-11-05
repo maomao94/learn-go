e02a2f3c376dd69ef3f4aa4f49876cd8b4cd19c5
hehanpeng <hanpeng.he@polixir.ai>
2020-11-05 17:33:32 +0800

    测试cityParse

--- crawler/zhenai/parser/citylist_test.go
+++ crawler/zhenai/parser/citylist_test.go
@@ -1,16 +1,44 @@
 package parser
 
 import (
-	"fmt"
-	"learn-go/crawler/fetcher"
+	"io/ioutil"
 	"testing"
 )
 
 func TestParseCityList(t *testing.T) {
-	contents, err := fetcher.Fetch("http://localhost:8080/mock/www.zhenai.com/zhenghun")
+	contents, err := ioutil.ReadFile("citylist_test_data.html")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n", contents)
-	ParseCityList(contents)
+	result := ParseCityList(contents)
+	const resultSize = 470
+	var expectedUrls = []string{
+		"http://localhost:8080/mock/www.zhenai.com/zhenghun/aba",
+		"http://localhost:8080/mock/www.zhenai.com/zhenghun/akesu",
+		"http://localhost:8080/mock/www.zhenai.com/zhenghun/alashanmeng",
+	}
+	var expectedCities = []string{
+		"阿坝", "阿克苏", "阿拉善盟",
+	}
+	if len(result.Request) != resultSize {
+		t.Errorf("result should have %d "+
+			"requests; but had %d", resultSize, len(result.Request))
+	}
+	for i, url := range expectedUrls {
+		if result.Request[i].Url != url {
+			t.Errorf("expected url #%d: %s; but "+
+				"was %s", i, url, result.Request[i].Url)
+		}
+	}
+	for i, city := range expectedCities {
+		if result.Items[i].(string) != city {
+			t.Errorf("expected url #%d: %s; but "+
+				"was %s", i, city, result.Items[i].(string))
+		}
+	}
+
+	if len(result.Items) != resultSize {
+		t.Errorf("result should have %d "+
+			"requests; but had %d", resultSize, len(result.Items))
+	}
 }
