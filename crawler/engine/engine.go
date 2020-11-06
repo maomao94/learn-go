4b1397b646f98ab6d1fd3d4e2eebd59a2a5610bb
hehanpeng <hanpeng.he@polixir.ai>
2020-11-06 16:14:09 +0800

    调整成并发

--- crawler/engine/engine.go
+++ crawler/engine/engine.go
@@ -14,17 +14,24 @@ func Run(seeds ...Request) {
 	for len(requests) > 0 {
 		r := requests[0]
 		requests = requests[1:]
-		log.Printf("Fetching %s", r.Url)
-		body, err := fetcher.Fetch(r.Url)
+		parseResult, err := worker(r)
 		if err != nil {
-			log.Printf("Fetcher: error "+
-				"fetching url %s: %v", r.Url, err)
 			continue
 		}
-		parseResult := r.ParserFunc(body)
 		requests = append(requests, parseResult.Request...)
 		for _, item := range parseResult.Items {
 			log.Printf("Got item %v", item)
 		}
 	}
+}
+
+func worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching %s", r.Url)
+	body, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("Fetcher: error "+
+			"fetching url %s: %v", r.Url, err)
+		return ParseResult{}, err
+	}
+	return r.ParserFunc(body), nil
 }
