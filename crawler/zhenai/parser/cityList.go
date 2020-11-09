3551c8f2a01d4f567b6375615a586b8f55fb930f
hehanpeng <hanpeng.he@polixir.ai>
2020-11-09 10:57:35 +0800

    调整成并发

--- crawler/zhenai/parser/cityList.go
+++ crawler/zhenai/parser/cityList.go
@@ -10,7 +10,7 @@ const cityListRe = `<a href="(http://localhost:8080/mock/www.zhenai.com/zhenghun
 func ParseCityList(contents []byte) engine.ParseResult {
 	re := regexp.MustCompile(cityListRe)
 	matches := re.FindAllSubmatch(contents, -1)
-	//limit := 1
+	limit := 1
 	result := engine.ParseResult{}
 	for _, m := range matches {
 		result.Items = append(result.Items, "City "+string(m[2]))
@@ -18,10 +18,10 @@ func ParseCityList(contents []byte) engine.ParseResult {
 			Url:        string(m[1]),
 			ParserFunc: ParseCity,
 		})
-		//limit--
-		//if limit == 0 {
-		//	break
-		//}
+		limit--
+		if limit == 0 {
+			break
+		}
 	}
 	return result
 }
