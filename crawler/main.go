8418dcd16cb5ab8990c3aad225a09823a6ae42fb
hehanpeng <hanpeng.he@polixir.ai>
2020-11-09 14:30:55 +0800

    调整成并发

--- crawler/main.go
+++ crawler/main.go
@@ -43,15 +43,15 @@ func main() {
 	//	ParserFunc: parser.ParseCityList,
 	//})
 
-	e := engine.ConcurrentEngine{
-		Scheduler:   &scheduler.SimpleScheduler{},
-		WorkerCount: 100,
-	}
-
 	//e := engine.ConcurrentEngine{
-	//	Scheduler:   &scheduler.QueuedScheduler{},
+	//	Scheduler:   &scheduler.SimpleScheduler{},
 	//	WorkerCount: 100,
 	//}
+
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.QueuedScheduler{},
+		WorkerCount: 100,
+	}
 
 	e.Run(engine.Request{
 		Url:        "http://localhost:8080/mock/www.zhenai.com/zhenghun",
