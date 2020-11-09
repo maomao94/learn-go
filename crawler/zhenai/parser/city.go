c202e32a9e6fbfd8b52f02c583e7f473d0856bd6
hehanpeng <hanpeng.he@polixir.ai>
2020-11-09 15:18:21 +0800

    去重

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -5,11 +5,15 @@ import (
 	"regexp"
 )
 
-const cityRe = `<a href="(http://localhost:8080/mock/album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
+var (
+	profileRe = regexp.MustCompile(
+		`<a href="(http://localhost:8080/mock/album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
+	cityUrlRe = regexp.MustCompile(
+		`href="(http://localhost:8080/mock/www.zhenai.com/zhenghun/[^"]+)"`)
+)
 
 func ParseCity(contents []byte) engine.ParseResult {
-	re := regexp.MustCompile(cityRe)
-	matches := re.FindAllSubmatch(contents, -1)
+	matches := profileRe.FindAllSubmatch(contents, -1)
 
 	result := engine.ParseResult{}
 	for _, m := range matches {
@@ -22,6 +26,15 @@ func ParseCity(contents []byte) engine.ParseResult {
 			},
 			//ParserFunc: engine.NilParser,
 		})
+	}
+
+	matches = cityUrlRe.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		result.Requests = append(result.Requests,
+			engine.Request{
+				Url:        string(m[1]),
+				ParserFunc: ParseCity,
+			})
 	}
 	return result
 }
