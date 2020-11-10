d6bbab0e312c9742e8b35523f1147b3667ea8650
hehanpeng <hanpeng.he@polixir.ai>
2020-11-10 14:04:57 +0800

    存储数据

---
+++ crawler/frontend/starter.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"learn-go/crawler/frontend/controller"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(
+		http.Dir("crawler/frontend/view")))
+	http.Handle("/search",
+		controller.CreateSearchResultHandler(
+			"crawler/frontend/view/template.html"))
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		panic(err)
+	}
+}
