3bb89e0b2616c6b1a2a31726160898721b278995
hehanpeng <hanpeng.he@polixir.ai>
2020-11-10 16:38:44 +0800

    存储数据

---
+++ crawler_distributed/rpcsupport/rpcsupport.go
@@ -0,0 +1,33 @@
+package rpcsupport
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+func ServerRpc(host string, service interface{}) error {
+	rpc.Register(service)
+	listen, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		return err
+	}
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Printf("accept error: %v", err)
+			continue
+		}
+		go jsonrpc.ServeConn(conn)
+	}
+	return nil
+}
+
+func NewClient(host string) (*rpc.Client, error) {
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		return nil, err
+	}
+	return jsonrpc.NewClient(conn), nil
+}
