5c5d58ced8c714458e7f5024bd068b2062abe28f
何汉鹏 <287737281@qq.com>
2021-03-15 10:43:35 +0800

    新增mqtt go 语言实现

--- mqtt/mqtt_server.go
+++ mqtt/mqtt_server.go
@@ -1,4 +1,4 @@
-package mqtt_server
+package main
 
 import (
 	"fmt"
@@ -14,9 +14,10 @@ var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 }
 
 func main() {
+	s := make(chan os.Signal, 1)
 	mqtt.DEBUG = log.New(os.Stdout, "", 0)
 	mqtt.ERROR = log.New(os.Stdout, "", 0)
-	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("emqx_test_client")
+	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("emqx_go_client")
 
 	opts.SetKeepAlive(60 * time.Second)
 	// 设置消息回调处理函数
@@ -29,24 +30,35 @@ func main() {
 	}
 
 	// 订阅主题
-	if token := c.Subscribe("testtopic/#", 0, nil); token.Wait() && token.Error() != nil {
+	if token := c.Subscribe("test/#", 0, nil); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
 
-	// 发布消息
-	token := c.Publish("testtopic/1", 0, false, "Hello World")
-	token.Wait()
+	go func() {
+		// 发布消息
+		c.Publish("test/1", 0, false, "Hello World")
+		c.Publish("test/2", 0, false, "Hello World")
+		c.Publish("test/3", 0, false, "Hello World")
+		c.Publish("test/4", 0, false, "Hello World")
+		c.Publish("test/5", 0, false, "Hello World")
+	}()
+	<-s
+	fmt.Println("信号：", s)
 
-	time.Sleep(6 * time.Second)
-
-	// 取消订阅
-	if token := c.Unsubscribe("testtopic/#"); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
-	}
-
-	// 断开连接
-	c.Disconnect(250)
-	time.Sleep(1 * time.Second)
+	//// 发布消息
+	//token := c.Publish("test/1", 0, false, "Hello World")
+	//token.Wait()
+	//
+	//time.Sleep(6 * time.Second)
+	//
+	//// 取消订阅
+	//if token := c.Unsubscribe("testtopic/#"); token.Wait() && token.Error() != nil {
+	//	fmt.Println(token.Error())
+	//	os.Exit(1)
+	//}
+	//
+	//// 断开连接
+	//c.Disconnect(250)
+	//time.Sleep(1 * time.Second)
 }
