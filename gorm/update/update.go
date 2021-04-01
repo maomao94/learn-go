aae84ae73354c4e22d515f78f64bdfcb3a9c3735
何汉鹏 <287737281@qq.com>
2021-04-01 15:53:54 +0800

    gorm 检索

--- gorm/update/update.go
+++ gorm/update/update.go
@@ -1,17 +1,34 @@
 package main
 
-import "learn-go/gorm/model"
+import (
+	"fmt"
+	"learn-go/gorm/model"
+)
 
 func main() {
 	db := model.Gorm()
 
-	//更新
+	// 更新
 
-	//保存所有字段
+	// 保存所有字段
 	var user model.User
 	db.First(&user)
 	user.Name = "jinzhu 2"
 	user.Age = 100
 	db.Save(&user)
 	// UPDATE users SET name='jinzhu 2', age=100, birthday='2016-01-01', updated_at = '2013-11-17 21:34:10' WHERE id=111;
+
+	// 更新单个列
+	// 条件更新
+	db.Model(&model.User{}).Where("active = ?", true).Update("name", "hello")
+	// UPDATE users SET name='hello', updated_at='2013-11-17 21:34:10' WHERE active=true;
+
+	// User 的 ID 是 `111`
+	db.Model(&user).Update("name", "hello")
+	// UPDATE users SET name='hello', updated_at='2013-11-17 21:34:10' WHERE id=111;
+
+	// 根据条件和 model 的值进行更新
+	db.Model(&user).Where("active = ?", true).Update("name", "hello")
+	// UPDATE users SET name='hello', updated_at='2013-11-17 21:34:10' WHERE id=111 AND active=true;
+	fmt.Println("complete")
 }
