aa40fa0f77fc09b6af33aeecafcaa11c4287c1c5
何汉鹏 <287737281@qq.com>
2021-04-01 16:26:11 +0800

    gorm 更新

--- gorm/update/update.go
+++ gorm/update/update.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"learn-go/gorm/model"
+	"time"
+
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -30,5 +33,39 @@ func main() {
 	// 根据条件和 model 的值进行更新
 	db.Model(&user).Where("active = ?", true).Update("name", "hello")
 	// UPDATE users SET name='hello', updated_at='2013-11-17 21:34:10' WHERE id=111 AND active=true;
+
+	// 更新多列
+	// 根据 `struct` 更新属性，只会更新非零值的字段
+	db.Model(&user).Updates(model.User{Name: "hello", Age: 18})
+	// UPDATE users SET name='hello', age=18, updated_at = '2013-11-17 21:34:10' WHERE id = 111;
+
+	// 根据 `map` 更新属性
+	db.Model(&user).Updates(map[string]interface{}{"name": "hello", "age": 18})
+	// UPDATE users SET name='hello', age=18, active=false, updated_at='2013-11-17 21:34:10' WHERE id=111;
+
+	// 更新选定字段
+	// 使用 Map 进行 Select
+	// User's ID is `111`:
+	db.Model(&user).Select("name").Updates(map[string]interface{}{"name": "hello", "age": 18})
+	// UPDATE users SET name='hello' WHERE id=111;
+
+	db.Model(&user).Omit("name").Updates(map[string]interface{}{"name": "hello", "age": 18})
+	// UPDATE users SET age=18, active=false, updated_at='2013-11-17 21:34:10' WHERE id=111;
+
+	// 使用 Struct 进行 Select（会 select 零值的字段）
+	db.Model(&user).Select("Name", "Age").Updates(model.User{Name: "new_name", Age: 0})
+	// UPDATE users SET name='new_name', age=0 WHERE id=111;
+
+	// Select 所有字段（查询包括零值字段的所有字段）
+	db.Model(&user).Select("*").Updates(model.User{Name: "jinzhu", Birthday: time.Now(), Age: 0, Model: gorm.Model{
+		CreatedAt: time.Now(),
+		ID:        user.ID,
+	}})
+
+	// Select 除 Role 外的所有字段（包括零值字段的所有字段）
+	db.Model(&user).Select("*").Omit("Birthday").Updates(model.User{Name: "jinzhu", Birthday: time.Now(), Age: 0, Model: gorm.Model{
+		CreatedAt: time.Now(),
+		ID:        user.ID,
+	}})
 	fmt.Println("complete")
 }
