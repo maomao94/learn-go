ab3825eeb2ab901e91efccb5e481c55405a25e5f
何汉鹏 <287737281@qq.com>
2021-04-01 17:12:32 +0800

    gorm 删除

--- gorm/delete/delete.go
+++ gorm/delete/delete.go
@@ -1,8 +1,24 @@
 package main
 
-import "learn-go/gorm/model"
+import (
+	"fmt"
+	"learn-go/gorm/model"
+
+	"gorm.io/gorm"
+)
 
 func main() {
 	db := model.Gorm()
 
+	// Email 的 ID 是 `10`
+	user := model.User{Model: gorm.Model{ID: 166}}
+
+	db.Delete(&user)
+	// DELETE from emails where id = 10;
+
+	// 带额外条件的删除
+	db.Where("name = ?", "jinzhu").Delete(&user)
+	// DELETE from emails where id = 10 AND name = "jinzhu";
+
+	fmt.Println("complete")
 }
