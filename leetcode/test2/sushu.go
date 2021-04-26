537917179d4e3881646ed8609f2ff1741b0b046b
何汉鹏 <287737281@qq.com>
2021-04-26 12:00:18 +0800

    leetcode

--- leetcode/test2/sushu.go
+++ leetcode/test2/sushu.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func bf(n int) int {
+	var count = 0
+	for i := 2; i < n; i++ {
+		if ok := isPrime(i); ok {
+			count = count + 1
+		}
+	}
+	return count
+}
+
+func isPrime(x int) bool {
+	for i := 2; i < x; i++ {
+		if x%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(bf(100))
 }
