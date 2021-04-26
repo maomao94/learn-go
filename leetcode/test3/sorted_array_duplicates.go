45697aec4667534454e09da135683251f4abefb6
何汉鹏 <287737281@qq.com>
2021-04-26 14:46:26 +0800

    leetcode

---
+++ leetcode/test3/sorted_array_duplicates.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
+
+func main() {
+	fmt.Println(
+		removeDuplicates([]int{0, 1}))
+}
