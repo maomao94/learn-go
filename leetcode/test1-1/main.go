94a25d2a06e5b0d7d9261d027eb3f2d0235e696e
何汉鹏 <287737281@qq.com>
2021-04-26 10:54:00 +0800

    leetcode

---
+++ leetcode/test1-1/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+func iterate(headNode *Node) {
+	var prev, next, cur *Node
+	// 当前标记头节点
+	cur = headNode
+	for {
+		// 当标记节点的next为nil时，代表循环结束
+		if cur == nil {
+			return
+		} else {
+			// 将标记节点的next留存
+			next = cur.Next
+			// 将标记节点的下一个设置成前一个节点
+			cur.Next = prev
+			// 将标记节点设置成前一个节点
+			prev = cur
+			// 将标记节点向后移动一个
+			cur = next
+		}
+	}
+}
+
+func main() {
+	var node5 = Node{5, nil}
+	var node4 = Node{4, &node5}
+	var node3 = Node{3, &node4}
+	var node2 = Node{2, &node3}
+	var node1 = Node{1, &node2}
+	var cur = &node1
+	for {
+		fmt.Println(cur.Value)
+		if cur.Next == nil {
+			break
+		} else {
+			cur = cur.Next
+		}
+	}
+	iterate(&node1)
+	fmt.Println("反转后")
+	cur = &node5
+	for {
+		fmt.Println(cur.Value)
+		if cur.Next == nil {
+			break
+		} else {
+			cur = cur.Next
+		}
+	}
+
+}
