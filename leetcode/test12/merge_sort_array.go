1b3efc537d11d9d2ba40d9928afd3588b61c8091
何汉鹏 <287737281@qq.com>
2021-04-28 16:33:14 +0800

    leetcode

--- leetcode/test12/merge_sort_array.go
+++ leetcode/test12/merge_sort_array.go
@@ -31,9 +31,29 @@ func merge(nums1 []int, m int, nums2 []int, n int) []int {
 	return nums1
 }
 
+// 双指针  go语言copy方法的局限性
+func merge2(nums1 []int, m int, nums2 []int, n int) []int {
+	p1, p2 := m-1, n-1 //指向temp,nums2
+	p := m + n - 1
+	for i := p; i >= 0; i-- {
+		if p1 >= 0 && p2 >= 0 {
+			break
+		}
+		if nums1[p1] < nums2[p2] {
+			nums1[i] = nums2[p2]
+			p2--
+		} else {
+			nums1[i] = nums1[p1]
+			p1--
+		}
+	}
+	return nums1
+}
+
 // 合并两个有序数组
 func main() {
-	nums1 := []int{1, 3, 5, 7, 9, 0, 0, 0, 0, 0}
+	nums1 := []int{1, 3, 5, 7, 13, 0, 0, 0, 0, 0}
 	nums2 := []int{2, 4, 6, 8, 10}
 	fmt.Println(merge(nums1, 5, nums2, 5))
+	fmt.Println(merge2(nums1, 5, nums2, 5))
 }
