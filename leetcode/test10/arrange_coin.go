4eb058beacf439ef84dfdf92af13ef065af85d32
何汉鹏 <287737281@qq.com>
2021-04-28 10:50:36 +0800

    leetcode

--- leetcode/test10/arrange_coin.go
+++ leetcode/test10/arrange_coin.go
@@ -12,7 +12,25 @@ func arrangeCoins(n int) int {
 	return -1
 }
 
+// 二分查找
+func arrangeCoins2(n int) int {
+	low, high := 0, n
+	for low <= high {
+		mid := (high-low)/2 + low
+		cost := ((mid + 1) * mid) / 2
+		if cost == n {
+			return mid
+		} else if cost > n {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
 // 排列硬币-三种解法
 func main() {
 	fmt.Println(arrangeCoins(10))
+	fmt.Println(arrangeCoins2(10))
 }
