499e5ece731816b48ef87bb4c99d15d504c2e142
何汉鹏 <287737281@qq.com>
2021-05-06 14:38:48 +0800

    省份数量

--- leetcode/test19/group_merge.go
+++ leetcode/test19/group_merge.go
@@ -1,9 +1,14 @@
 package main
 
+import "fmt"
+
+// 深度优先
 func getProvince(citys [][]int) int {
 	return 0
 }
 
+// 省份数量
 func main() {
-
+	fmt.Println(getProvince([][]int{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}})) // 2
+	fmt.Println(getProvince([][]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}})) // 3
 }
