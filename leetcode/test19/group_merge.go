5e1112a57026863354ab8f82f2f0f64f8b0038f3
何汉鹏 <287737281@qq.com>
2021-05-07 14:24:58 +0800

    省份数量

--- leetcode/test19/group_merge.go
+++ leetcode/test19/group_merge.go
@@ -46,6 +46,54 @@ func bfs(citysConnected [][]int) int {
 }
 
 // 并查集
+func mergeFind(citysConnected [][]int) int {
+	citys := len(citysConnected)
+	head := make([]int, citys)
+	level := make([]int, citys)
+	for i := 0; i < citys; i++ {
+		head[i] = i
+		level[i] = i
+	}
+	for i := 0; i < citys; i++ {
+		for j := i + 1; j < citys; j++ {
+			if citysConnected[i][j] == 1 {
+				merge(i, j, head, level)
+			}
+		}
+	}
+	count := 0
+	for i := 0; i < citys; i++ {
+		if head[i] == i {
+			count++
+		}
+	}
+	return count
+}
+
+func merge(x int, y int, head []int, level []int) {
+	i := find(x, head)
+	j := find(y, head)
+	if i == j {
+		return
+	}
+	if level[i] <= level[j] {
+		head[i] = j
+	} else {
+		head[j] = i
+	}
+	if level[i] == level[j] {
+		level[i]++
+		level[j]++
+	}
+}
+
+func find(x int, head []int) int {
+	if head[x] == x {
+		return x
+	}
+	head[x] = find(head[x], head)
+	return head[x]
+}
 
 // 深度优先
 func dfs(i int, citys int, visited []bool, citysConnected [][]int) {
@@ -63,4 +111,6 @@ func main() {
 	fmt.Println(getProvince([][]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}})) // 3
 	fmt.Println(bfs([][]int{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}))         // 2
 	fmt.Println(bfs([][]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}))         // 3
+	fmt.Println(mergeFind([][]int{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}))   // 2
+	fmt.Println(mergeFind([][]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}))   // 3
 }
