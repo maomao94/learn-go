308cae4a7bb9f78b53fcd4ca01f4bafe72fedb89
何汉鹏 <287737281@qq.com>
2021-05-07 13:44:18 +0800

    省份数量

--- leetcode/test19/group_merge.go
+++ leetcode/test19/group_merge.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/chentaihan/container/queue"
+)
 
 // 深度优先
 func getProvince(citysConnected [][]int) int {
@@ -11,6 +15,30 @@ func getProvince(citysConnected [][]int) int {
 		if !visited[i] {
 			// 深度优先
 			dfs(i, citys, visited, citysConnected)
+			province++
+		}
+	}
+	return province
+}
+
+// 广度优先
+func bfs(citysConnected [][]int) int {
+	citys := len(citysConnected)
+	visited := make([]bool, citys)
+	province := 0 // 计数器
+	queueLink := queue.NewQueueLink()
+	for i := 0; i < citys; i++ {
+		if !visited[i] {
+			queueLink.Enqueue(i)
+			for !queueLink.Empty() {
+				k, _ := queueLink.Dequeue()
+				visited[k.(int)] = true
+				for j := 0; j < citys; j++ {
+					if citysConnected[i][j] == 1 && !visited[j] {
+						queueLink.Enqueue(j)
+					}
+				}
+			}
 			province++
 		}
 	}
@@ -30,4 +58,6 @@ func dfs(i int, citys int, visited []bool, citysConnected [][]int) {
 func main() {
 	fmt.Println(getProvince([][]int{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}})) // 2
 	fmt.Println(getProvince([][]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}})) // 3
+	fmt.Println(bfs([][]int{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}))         // 2
+	fmt.Println(bfs([][]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}))         // 3
 }
