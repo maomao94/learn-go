cc270373039a56e85ee31260771862a8983b5519
hehanpeng <287737281@qq.com>
2021-08-23 11:05:39 +0800

    generic study

---
+++ generic/05/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Price int
+
+func (i Price) String() string {
+	return strconv.Itoa(int(i))
+}
+
+type ShowPrice interface {
+	type int, int8, int16, int32, int64
+	String() string
+}
+
+func showPriceList[T ShowPrice](s []T) (ret []string) {
+	for _, v := range s {
+		ret = append(ret, v.String())
+	}
+	return ret
+}
+
+// 使用interface约束泛型
+// go run -gcflags=-G=3 main.go
+func main() {
+	fmt.Println(showPriceList([]Price{48, 88, 152, 219, 328}))
+}
