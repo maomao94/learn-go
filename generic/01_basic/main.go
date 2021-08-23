c6fe600c959444614244685ca2e238a52a42f8c7
hehanpeng <287737281@qq.com>
2021-08-23 10:45:05 +0800

    generic study

---
+++ generic/01_basic/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func printSlice[T any](s []T) {
+	for _, v := range s {
+		fmt.Printf("%v ", v)
+	}
+	fmt.Print("\n")
+}
+
+//go run -gcflags=-G=3 main.go
+func main() {
+	printSlice[int]([]int{66, 77, 88, 99, 100})
+	printSlice[float64]([]float64{1.1, 2.2, 3.3, 4.4, 5.5})
+	printSlice[string]([]string{"红烧肉", "清蒸鱼", "大闸蟹", "九转大肠", "重烧海参"})
+	printSlice([]int64{55, 44, 33, 22, 11})
+}
