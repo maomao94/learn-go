327df2790cbbdf817814788451939a33c3bd6447
hehanpeng <287737281@qq.com>
2021-08-23 10:53:05 +0800

    generic study

---
+++ generic/03/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Addable interface {
+	type int, int8, int16, int32, int64, uint, uint8, uint16, uint32,
+		uint64, uintptr, float32, float64, complex64, complex128, string
+}
+
+func add[T Addable] (a, b T) T { return a + b }
+
+// 泛型函数
+// go run -gcflags=-G=3 main.go
+func main() {
+	fmt.Println(add(3,4))
+	fmt.Println(add("Go","lang"))
+}
