f5ff4a1bc1c192fbf82db786c49336c5478b992c
hehanpeng <287737281@qq.com>
2022-06-11 09:21:39 +0800

    测试

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -3,22 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/zeromicro/go-zero/core/mr"
-	"time"
 )
 
 func main() {
-	var a [10]int
-	for i := 0; i < 100000; i++ {
-		go func(i int) {
-			for {
-				//a[i]++
-				//runtime.Gosched() //主动交出控制权
-				fmt.Printf("Hello from goroutine %d\n", i)
-			}
-		}(i)
-	}
-	time.Sleep(time.Minute)
-	fmt.Println(a)
+	//var a [10]int
+	//for i := 0; i < 100000; i++ {
+	//	go func(i int) {
+	//		for {
+	//			//a[i]++
+	//			//runtime.Gosched() //主动交出控制权
+	//			fmt.Printf("Hello from goroutine %d\n", i)
+	//		}
+	//	}(i)
+	//}
+	//time.Sleep(time.Minute)
+	//fmt.Println(a)
 
 	// mapreduce
 	var uids []int64
@@ -33,7 +32,7 @@ func main() {
 		return nil
 	})
 	fmt.Println(uids)
-
+	fmt.Println("##########")
 	var uidsR []int64
 	v, _ := mr.MapReduce(func(source chan<- interface{}) {
 		for i := range uids {
